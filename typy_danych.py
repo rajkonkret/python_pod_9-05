2459d26fa95586ff065320e11d2c2bf84f812f78
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-09 11:55:24 +0200

    update typy_danych

--- typy_danych.py
+++ typy_danych.py
@@ -22,3 +22,42 @@ print(wiek ** rok)  # potęgowanie
 print(54 - (5 * 42) + (4 / 2 + 4) / 2)
 print("\tSprawdzam zmienną temp: {} {}\n".format(wiek, temp))
 # 11:30
+print(f"\tSprawdzam zmienną wiek: {wiek} {temp}\n")
+print(f"""
+    zmienna {wiek}
+    zmienna {temp}
+    wynik działania:
+    {54 - (5 * 43) + (4 / 2 + 4) / 2}
+""")
+imie = "Radek Radek"
+imie.lower()  # zamiana na małe litery
+print(imie)
+imie2 = imie.lower()
+print(imie2)
+# upper, cpitalize
+print(imie2.upper())
+print(imie2.capitalize())
+print(imie2)
+
+# czesc całkowita z dzielenia
+print(4 // 2)  # int
+print(type(4 // 2))  # int
+# dzielenie z ulamkiem
+print(4 / 2)  # float
+print(type(4 / 2))  # float
+print(type(5 / 2))  # float
+
+# zmienna logiczna
+# typ logiczny
+# True , False
+czy_znasz_Pythona = True  # prawda
+print(czy_znasz_Pythona)
+czy_znasz_Pythona = False
+print(czy_znasz_Pythona)
+print(type(czy_znasz_Pythona))  # <class 'bool'>
+print(int(czy_znasz_Pythona))  # 0 - False
+print(int(True))  # 1 - True
+print(bool(0))  # False
+print(bool(1))  # True
+print(bool(100))  # True
+print(bool(-100))  # True
