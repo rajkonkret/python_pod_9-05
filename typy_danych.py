7e284931977dc5ef2aede956128d4a41bc97fbfa
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-09 11:34:50 +0200

    aad typy_danych

---
+++ typy_danych.py
@@ -0,0 +1,24 @@
+wiek = 47  # int - liczby całkowite
+rok = 2023
+temp = 36.6  # float - zmiennoprzecinkowy
+wiek2 = 37.5
+
+# print(wiek)
+# print(type(wiek))
+# print(wiek2)
+# print(type(wiek2))
+
+print(5 * "Radek")
+print(5 * "2")
+print(5 * int("2"))  # rzutowanie na int
+
+print(wiek + rok)
+print(wiek - rok)
+print(wiek * rok)
+print(wiek // rok)  # czesc całkowita dzielenia
+print(wiek / rok)  # dzielenie ( zawsze zwraca float)
+print(type(wiek / rok))  # dzielenie ( zawsze zwraca float)
+print(wiek ** rok)  # potęgowanie
+print(54 - (5 * 42) + (4 / 2 + 4) / 2)
+print("\tSprawdzam zmienną temp: {} {}\n".format(wiek, temp))
+# 11:30
