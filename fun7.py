0173d3f40a9a7f03e0772f9ee95dc20653b7253a
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-11 08:47:15 +0200

    update fun7

--- fun7.py
+++ fun7.py
@@ -6,12 +6,19 @@ def connect(**opcje):
         'port': '8080'
     }
     print(connect_param)
+    # dodajemy klucz 'pwd' i nadajemy wartość {'a': 7}
     connect_param['pwd'] = opcje
+    # {'host': '127.0.0.7', 'port': '8080', 'pwd': {'a': 7}}
     print(connect_param)
+    # drugi sposób dodawania klucza i wartości do słownika
     connect_param.update({'pwd': opcje})
+    # {'host': '127.0.0.7', 'port': '8080', 'pwd': {'a': 7}}
+    # 'pwd': {'userr': '/home', 'root': '/', 'port': '9090'}}
     print(connect_param)
 
 
 connect(a=7)
 connect(user='/home')
+# {'userr': '/home', 'root': '/', 'port': '9090'}
 connect(userr='/home', root='/', port='9090')
+# 8:30
