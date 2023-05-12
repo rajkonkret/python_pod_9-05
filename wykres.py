2e5839c48c36a10b9cd2145ffc55ce020f4f8f34
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-12 11:40:48 +0200

    add matplotlib

---
+++ wykres.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 6, 8]
+
+plt.plot(x, y)
+plt.title("Wykres liniowy")
+plt.xlabel("Os X")
+plt.ylabel("Os Y")
+
+plt.show()
---
+++ wykres2-histogram.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy
+import numpy as np
+
+data = np.random.normal(0, 1, 1000)
+
+plt.hist(data, bins=30)
+plt.title("Histogram")
+plt.xlabel("Wartości")
+plt.ylabel("Częstość")
+
+plt.show()
