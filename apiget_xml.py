42d5fb0a8965e216ccf14f474ca7c5aef75c1603
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-12 08:39:26 +0200

    apiget xml

--- apiget_xml.py
+++ apiget_xml.py
@@ -8,11 +8,17 @@ xml_data = response.content
 
 root = ET.fromstring(xml_data)
 print(xml_data)
-table_name = root.find('.//ExchangeRatesTable')
-date = root.find('.//EffectiveDate')
-currrency = table_name.findall(".//Currency")
-
-print(table_name)
-print(currrency)
-print(currrency[0])
+table_name = root.find('.//Table').text
+date = root.find('.//EffectiveDate').text
+rates = root.findall('.//Rate')
 print(date)
+print(table_name)
+for rate in rates:
+    currency = rate.find('Currency').text
+    code = rate.find('Code').text
+    mid = rate.find('Mid').text
+    print(f"{code}: {currency} - {mid}")
+# print(table_name)
+# print(currrency)
+# print(currrency[0])
+# print(date)
--- start.py
+++ start.py
@@ -6,3 +6,5 @@ from pakiet import fun_pakiet
 pk.powitanie()
 fun_pakiet.powitanie()
 pakiet.fun_pakiet.powitanie()
+# 8:30
+
