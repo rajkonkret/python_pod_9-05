313ce873cff2199f54aec8c6c5a2b24338c91f94
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-12 09:34:01 +0200

    add xml 4

---
+++ xml_test_4.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+
+
+def generateXML(filename):
+    root = ET.Element("Catalog")
+
+    m1 = ET.Element("mobile")
+    root.append(m1)
+
+    b1 = ET.SubElement(m1, "brand")
+    b1.text = "Redmi"
+    b2 = ET.SubElement(m1, "price")
+    b2.text = "6999"
+
+    m2 = ET.Element('mobile')
+    root.append(m2)
+
+    tree = ET.ElementTree(root)
+
+    with open(filename, "wb") as file:
+        tree.write(file)
+
+
+if __name__ == "__main__":
+    generateXML("Catalog.xml")
